2c70e5b88dcd405ab5de5cca6b9dfd99a1ff6807
pramje-drum <pramje@trueigtech.com>
2026-02-20 21:30:29 +0530

    Update

--- src/app/(main)/dashboard/page.jsx
+++ src/app/(main)/dashboard/page.jsx
@@ -3,7 +3,11 @@ import Dashboard from "@/components/dashboard/Index";
 export const metadata = {
 	title: "Dashboard | ProductBoard",
 	description: " dashboard for buying products.",
-	author:[{name: "Piyush Ramje"}]
+	authors: [
+		{ name: "Piyush Ramje" },
+		{ name: "DrumKarma", url: "www.youtube.com" },
+	],
+	keywords: ["nextjs", "products ", "flipkart", "amazon", "blinkit"],
 };
 
 export default function DashboardPage() {
